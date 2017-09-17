da5e25752cdc54d73b203666f1e7f1fcdaa89bfe
lyusiqi <31831942+lyusiqi@users.noreply.github.com>
2017-09-17 16:36:34 -0700

    Rename puzzle-4.js to puzzle-5.js

---
+++ puzzle-5.js
@@ -0,0 +1,22 @@
+function Right() {
+  right();
+  right();
+}
+function Color() {
+if (getColor() == "red") {
+  	up();
+}
+if (getColor () == "blue"){
+    down()
+}
+}
+function Get(){
+Right()
+Color()
+}
+Get()
+Get()
+Get()
+right()
+right()
+right()
