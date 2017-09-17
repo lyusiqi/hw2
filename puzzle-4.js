804dbf69cf5535d16446eeb8a068e9bdb905f6b1
lyusiqi <31831942+lyusiqi@users.noreply.github.com>
2017-09-17 15:59:22 -0700

    Rename puzzle-3.js to puzzle-4.js

---
+++ puzzle-4.js
@@ -0,0 +1,18 @@
+
+down();
+down();
+down()
+down()
+right()
+var firstColorSeen = getColor()
+down()
+setColor(firstColorSeen)
+right()
+right()
+up()
+right()
+var secondColorSeen = getColor()
+down()
+setColor(secondColorSeen);
+right()
+right()
