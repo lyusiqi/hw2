d71b93830bf5843efffb92bd7bad90387cced3dd
lyusiqi <31831942+lyusiqi@users.noreply.github.com>
2017-09-17 17:36:49 -0700

    Create puzzle-7.js

---
+++ puzzle-7.js
@@ -0,0 +1,28 @@
+function Go() {
+while (getColor() != "red") {
+  down();
+}
+if (getColor() =="red") {
+  right()
+  right()
+}
+}
+function Up() {
+while (getColor() != "red") {
+  up()
+}
+  if (getColor() =="red") {
+  right()
+  right()
+}
+}
+Go()
+Up()
+Go()
+Up()
+while (getColor() != "red") {
+  down()
+}
+if (getColor () =="red"){
+  right()
+}
